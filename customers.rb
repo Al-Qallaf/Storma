df1c8fb4c46970cd99798c9dcff245602ea10058
Ali Hameed Al-Qallaf <ahhahalqalaf@dundee.ac.uk>
2012-10-10 21:50:32 +0100

    project main file created, generated files into folders, and other general refactoring

--- customers.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'active_record'
-
-class Customers < ActiveRecord::Base
-	self.table_name = "customers"
-	self.primary_key = "idcustomers"
-	has_many :orders
-end
--- orders.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'active_record'
-
-class Orders < ActiveRecord::Base
-	self.table_name = "orders"
-	self.primary_key = "idorders"
-	belongs_to :customers, :foreign_key => "customerid"
-end
